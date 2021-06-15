f9e1ca86358e58694fd10925d80552201dbd03f3
Peter <peterrose03@gmail.com>
2021-06-15 17:41:58 +0200

    required_providers

--- Avalon-Teraform/main.tf
+++ Avalon-Teraform/main.tf
@@ -1,7 +1,11 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used 
 terraform {
-  backend "azurerm" {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.56.0"
+    }
   }
 }
 
