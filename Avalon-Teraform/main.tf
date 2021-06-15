d12e2c0d86742370691fe09c011017d3417d1ecc
Peter <peterrose03@gmail.com>
2021-06-15 17:48:37 +0200

    features {}

--- Avalon-Teraform/main.tf
+++ Avalon-Teraform/main.tf
@@ -12,6 +12,10 @@ terraform {
 # Configure the Azure provider
 provider "azurerm" {
     features {}
+    subscription_id = "${var.subscription_id}"
+    client_id       = "${var.client_id}"
+    client_secret   = "${var.client_secret}"
+    tenant_id       = "${var.tenant_id}"
 }
 
 # Generate random text for a unique name
