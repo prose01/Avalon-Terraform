34d4c8cde504d823c1929c1055b2caafbc3d2aec
Peter <peterrose03@gmail.com>
2021-06-15 18:03:31 +0200

    skip_provider_registration = true

--- Avalon-Teraform/main.tf
+++ Avalon-Teraform/main.tf
@@ -1,21 +1,18 @@
 # We strongly recommend using the required_providers block to set the
 # Azure Provider source and version being used 
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "2.46.0"
-    }
+  backend "azurerm" {
   }
 }
 
 # Configure the Azure provider
 provider "azurerm" {
-    features {}
+    skip_provider_registration = true
     subscription_id = "${var.subscription_id}"
     client_id       = "${var.client_id}"
     client_secret   = "${var.client_secret}"
     tenant_id       = "${var.tenant_id}"
+    features {}
 }
 
 # Generate random text for a unique name
