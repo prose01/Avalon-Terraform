e60d357028817fbd46ec0db54f29d1a6d7d177d7
Peter <peterrose03@gmail.com>
2021-06-15 17:46:38 +0200

    required_providers

--- Avalon-Teraform/main.tf
+++ Avalon-Teraform/main.tf
@@ -4,17 +4,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.56.0"
+      version = "2.46.0"
     }
   }
 }
 
 # Configure the Azure provider
 provider "azurerm" {
-    subscription_id = "${var.subscription_id}"
-    client_id       = "${var.client_id}"
-    client_secret   = "${var.client_secret}"
-    tenant_id       = "${var.tenant_id}"
     features {}
 }
 
