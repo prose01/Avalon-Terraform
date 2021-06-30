c7fa306126bda2aa35907a0e3d6b9f5e491aaaf2
Peter <peterrose03@gmail.com>
2021-06-30 15:50:56 +0200

    Linux App service

--- Avalon-Teraform/main.tf
+++ Avalon-Teraform/main.tf
@@ -15,33 +15,33 @@ provider "azurerm" {
     features {}
 }
 
-# Generate random text for a unique name
-resource "random_id" "randomId" {
-    keepers = {
-        # Generate a new ID only when a new resource group is defined
-        resource_group = azurerm_resource_group.myterraformgroup.name
-    }
+# # Generate random text for a unique name
+# resource "random_id" "randomId" {
+#     keepers = {
+#         # Generate a new ID only when a new resource group is defined
+#         resource_group = azurerm_resource_group.myterraformgroup.name
+#     }
 
-    byte_length = 8
-}
+#     byte_length = 8
+# }
 
 # Create resource group
-resource "azurerm_resource_group" "myterraformgroup" {
-    name     = "myResourceGroup-${var.sourceBranchName}"
+resource "azurerm_resource_group" "avalon-group" {
+    name     = "Avalon-ResourceGroup-${var.sourceBranchName}"
     location = "${var.location}"
 
     tags = {
-        environment = "Terraform demo01"
-        build       = "demo01"
-        myterraformgroup = "${var.sourceBranchName}"
+        Avalon = "${var.sourceBranchName}"
     }
 }
 
 # Create app service plan
-resource "azurerm_app_service_plan" "plandemo" {
-    name                = "slotAppServicePlan-${var.sourceBranchName}"
-    location            = azurerm_resource_group.myterraformgroup.location
-    resource_group_name = azurerm_resource_group.myterraformgroup.name
+resource "azurerm_app_service_plan" "avalon-plan" {
+    name                = "Avalon-AppServicePlan-${var.sourceBranchName}"
+    location            = azurerm_resource_group.avalon-group.location
+    resource_group_name = azurerm_resource_group.avalon-group.name
+    kind                = "Linux"
+    reserved            = true
     
     sku {
         tier = "Standard"
@@ -49,23 +49,26 @@ resource "azurerm_app_service_plan" "plandemo" {
     }
 
     tags = {
-        environment = azurerm_resource_group.myterraformgroup.tags.environment
-        build       = azurerm_resource_group.myterraformgroup.tags.build
-        myterraformgroup = azurerm_resource_group.myterraformgroup.tags.myterraformgroup
+        Avalon = azurerm_resource_group.myterraformgroup.tags.Avalon
     }
 }
 
 # Create app service
-resource "azurerm_app_service" "servicedemo" {
-    name                = "${random_id.randomId.hex}slotAppService-${var.sourceBranchName}"
-    location            = azurerm_resource_group.myterraformgroup.location
-    resource_group_name = azurerm_resource_group.myterraformgroup.name
-    app_service_plan_id = azurerm_app_service_plan.plandemo.id
+resource "azurerm_app_service" "avalon" {
+    name                = "Avalon-${var.sourceBranchName}"
+    location            = azurerm_resource_group.avalon-group.location
+    resource_group_name = azurerm_resource_group.avalon-group.name
+    app_service_plan_id = azurerm_app_service_plan.avalon-plan.id
 
     site_config {
         dotnet_framework_version = "v5.0"
-        remote_debugging_enabled = true
-        remote_debugging_version = "VS2019"
+        # remote_debugging_enabled = true
+        # remote_debugging_version = "VS2019"
+        always_on = "true"
+        ftps_state = "FtpsOnly"
+        http2_enabled = "true"
+        use_32_bit_worker_process = "false"
+        min_tls_version = "1.2"
     }
 
     app_settings = {
@@ -78,33 +81,74 @@ resource "azurerm_app_service" "servicedemo" {
     #     value = "Server=tcp:demosqlserver.database.windows.net,1433;Initial Catalog=demosqldatabase;Persist Security Info=False;User ID=${var.administrator_login};Password=${var.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
     # }
 
-    tags = {
-        environment = azurerm_resource_group.myterraformgroup.tags.environment
-        build       = azurerm_resource_group.myterraformgroup.tags.build
-        myterraformgroup = azurerm_resource_group.myterraformgroup.tags.myterraformgroup
+    https_only = "true"
+
+    identity {
+        type = "SystemAssigned"
+    }
+
+    logs {
+        application_logs {
+            file_system {
+                quota = 30     # in Megabytes
+                retention_period = 30     # in days
+            }
+        }
+    }
+
+    tags = {       
+        Avalon = azurerm_resource_group.myterraformgroup.tags.Avalon
     }
 }
 
-# Create app service slot
-resource "azurerm_app_service_slot" "slotdemo" {
-    name                = "${random_id.randomId.hex}slotAppServiceSlotOne-${var.sourceBranchName}"
-    location            = azurerm_resource_group.myterraformgroup.location
-    resource_group_name = azurerm_resource_group.myterraformgroup.name
-    app_service_plan_id = azurerm_app_service_plan.plandemo.id
-    app_service_name    = azurerm_app_service.servicedemo.name
+# # Create app service slot
+# resource "azurerm_app_service_slot" "avalon-slot" {
+#     name                = "Avalon-staging-${var.sourceBranchName}"
+#     location            = azurerm_resource_group.avalon-group.location
+#     resource_group_name = azurerm_resource_group.avalon-group.name
+#     app_service_plan_id = azurerm_app_service_plan.avalon-plan.id
+#     app_service_name    = azurerm_app_service.avalon.name
 
-    site_config {
-        dotnet_framework_version = "v5.0"
-        remote_debugging_enabled = true
-        remote_debugging_version = "VS2019"
-    }
+#     site_config {
+#         dotnet_framework_version = "v5.0"
+#         # remote_debugging_enabled = true
+#         # remote_debugging_version = "VS2019"
+#         always_on = "true"
+#         ftps_state = "FtpsOnly"
+#         http2_enabled = "true"
+#         use_32_bit_worker_process = "false"
+#         min_tls_version = "1.2"
+#     }
 
-    tags = {
-        environment = azurerm_resource_group.myterraformgroup.tags.environment
-        build       = azurerm_resource_group.myterraformgroup.tags.build
-        myterraformgroup = azurerm_resource_group.myterraformgroup.tags.myterraformgroup
-    }
-}
+#     app_settings = {
+#         "SOME_KEY" = "some-value"
+#     }
+
+#     # connection_string {
+#     #     name  = "Database"
+#     #     type  = "SQLServer"
+#     #     value = "Server=tcp:demosqlserver.database.windows.net,1433;Initial Catalog=demosqldatabase;Persist Security Info=False;User ID=${var.administrator_login};Password=${var.administrator_login_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+#     # }
+
+#     https_only = "true"
+
+#     identity {
+#         type = "SystemAssigned"
+#     }
+
+#     logs {
+#         application_logs {
+#             file_system {
+#                 quota = 30     # in Megabytes
+#                 retention_period = 30     # in days
+#             }
+#         }
+#     }
+
+#     tags = {       
+#         Avalon = azurerm_resource_group.myterraformgroup.tags.Avalon
+#     }
+# }
 
 # Create storage account
 # resource "azurerm_storage_account" "mystorageaccount" {
